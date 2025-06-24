626d4e148c4e5927e1ea263811de293539a83aaf
iu <i.ueda@orz-he.com>
2025-06-24 23:19:59 +0900

    Add singleton creation to action factories

--- src/main/java/iu/LCAC/Actions/demos/change_center_color/ChangeCenterColorActionFactory.java
+++ src/main/java/iu/LCAC/Actions/demos/change_center_color/ChangeCenterColorActionFactory.java
@@ -6,9 +6,19 @@ import iu.LCAC.Framework.action.member.AbstActionMemberFactory;
 
 public class ChangeCenterColorActionFactory extends AbstActionMemberFactory {
 
+    private ChangeCenterColorAction action;
+
     @Override
     public AbstActionMember createAction(String action_name, String short_name ) {
-        return new ChangeCenterColorAction(action_name, short_name );
+        return createSingleton(action_name, short_name );
+    }
+
+    @Override
+    protected AbstActionMember createSingleton(String action_name, String short_name) {
+        if (action == null) {
+            action = new ChangeCenterColorAction(action_name, short_name);
+        }
+        return action;
     }
 
 }
--- src/main/java/iu/LCAC/Actions/demos/change_west_color/ChangeWestColorActionFactory.java
+++ src/main/java/iu/LCAC/Actions/demos/change_west_color/ChangeWestColorActionFactory.java
@@ -6,9 +6,18 @@ import iu.LCAC.Framework.action.member.AbstActionMember;
 
 public class ChangeWestColorActionFactory extends AbstActionMemberFactory {
 
+    private ChangeWestColorAction action;
 
     @Override
     public AbstActionMember createAction(String action_name, String short_name) {
-        return new ChangeWestColorAction(action_name, short_name);
+        return createSingleton(action_name, short_name);
+    }
+
+    @Override
+    protected AbstActionMember createSingleton(String action_name, String short_name) {
+        if (action == null) {
+            action = new ChangeWestColorAction(action_name, short_name);
+        }
+        return action;
     }
 }
